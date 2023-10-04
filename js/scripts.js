5df1c10cb53fb6b60552a76553e617caa5073643
mrfukui <mr.fukui@yahoo.com>
2023-10-03 19:00:51 -0700

    created a to-do-list using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function newItem() {
+
+    let inputValue = $('#input').val();
+    let li = $('<li></li>');
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+    }
+
+    li.on("dblclick", function() {
+        li.toggleClass("strike");
+    });
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", function() {
+        li.addClass("delete")
+    });
+
+    $('#list').sortable();
+}
